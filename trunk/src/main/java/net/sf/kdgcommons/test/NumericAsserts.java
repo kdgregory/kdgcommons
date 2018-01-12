f1ee2b8dc0d5bddc970047a062eb5bff4c6c4de6
kdgregory <kdgregory@e368c642-3b58-40b6-89f3-1596afd8f84b>
2018-01-12 13:14:28 +0000

    NumericAsserts: add assertApproximate() for long and double, assertInRange() for int, long, and double

--- trunk/src/main/java/net/sf/kdgcommons/test/NumericAsserts.java
+++ trunk/src/main/java/net/sf/kdgcommons/test/NumericAsserts.java
@@ -23,15 +23,68 @@ import junit.framework.Assert;
 public class NumericAsserts
 {
     /**
-     *  Asserts that the actual value is within the expected, plus/minus
-     *  the specified percentage.
+     *  Asserts that the actual value is within the expected, plus/minus the
+     *  specified percentage. This is a reasonable approach to probabilistic
+     *  testing.
      */
     public static void assertApproximate(int expected, int actual, int deltaPercent)
     {
         int delta = (int)(((long)expected * deltaPercent) / 100);
-        int loBound = expected - delta;
-        Assert.assertTrue("expected >= " + loBound + ", was " + actual, actual >= loBound);
-        int hiBound = expected + delta;
-        Assert.assertTrue("expected <= " + hiBound + ", was " + actual, actual <= hiBound);
+        assertInRange(expected - delta, expected + delta, actual);
+    }
+
+    /**
+     *  Asserts that the actual value is within the expected, plus/minus the
+     *  specified percentage. This is a reasonable approach to probabilistic
+     *  testing.
+     */
+    public static void assertApproximate(long expected, long actual, int deltaPercent)
+    {
+        // to avoid overflow, we swap the divide and multiply depending on the size of
+        // the value -- assumption is that range of error is minimal compared to delta
+        long delta = (expected > Integer.MAX_VALUE * 100)
+                   ? (expected / 100) * deltaPercent
+                   : (expected * deltaPercent) / 100;
+        assertInRange(expected - delta, expected + delta, actual);
+    }
+
+    /**
+     *  Asserts that the actual value is within the expected, plus/minus the
+     *  specified percentage. This is a reasonable approach to probabilistic
+     *  testing.
+     */
+    public static void assertApproximate(double expected, double actual, double deltaPercent)
+    {
+        // to avoid overflow, we swap the divide and multiply depending on the size of
+        // the value -- assumption is that range of error is minimal compared to delta
+        double delta = (expected * deltaPercent) / 100;
+        assertInRange(expected - delta, expected + delta, actual);
+    }
+
+    /**
+     *  Asserts that the actual value is within an arbitrary range +/- the expected value.
+     */
+    public static void assertInRange(int expectedLow, int expectedHigh, int actual)
+    {
+        Assert.assertTrue("expected >= " + expectedLow + ", was " + actual, actual >= expectedLow);
+        Assert.assertTrue("expected <= " + expectedHigh + ", was " + actual, actual <= expectedHigh);
+    }
+
+    /**
+     *  Asserts that the actual value is within an arbitrary range +/- the expected value.
+     */
+    public static void assertInRange(long expectedLow, long expectedHigh, long actual)
+    {
+        Assert.assertTrue("expected >= " + expectedLow + ", was " + actual, actual >= expectedLow);
+        Assert.assertTrue("expected <= " + expectedHigh + ", was " + actual, actual <= expectedHigh);
+    }
+
+    /**
+     *  Asserts that the actual value is within an arbitrary range +/- the expected value.
+     */
+    public static void assertInRange(double expectedLow, double expectedHigh, double actual)
+    {
+        Assert.assertTrue("expected >= " + expectedLow + ", was " + actual, actual >= expectedLow);
+        Assert.assertTrue("expected <= " + expectedHigh + ", was " + actual, actual <= expectedHigh);
     }
 }
