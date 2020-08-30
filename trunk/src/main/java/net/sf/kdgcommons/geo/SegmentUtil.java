c57ba396b79ce7b7ea2e54dd1daeaf7bf784de86
kdgregory <kdgregory@e368c642-3b58-40b6-89f3-1596afd8f84b>
2020-08-30 12:00:19 +0000

    add geo.SegmentUtil

--- trunk/src/main/java/net/sf/kdgcommons/geo/SegmentUtil.java
+++ trunk/src/main/java/net/sf/kdgcommons/geo/SegmentUtil.java
@@ -26,12 +26,12 @@ public class SegmentUtil
     /**
      *  Computes and sums the Pythagorean distance between the points in a segment.
      */
-    public static double distance(List<Point> segment)
+    public static double distance(List<? extends Point> segment)
     {
         if ((segment == null) || segment.isEmpty())
             return 0;
 
-        Iterator<Point> itx = segment.iterator();
+        Iterator<? extends Point> itx = segment.iterator();
         Point prev = itx.next();
         double sum = 0;
         while (itx.hasNext())
@@ -49,14 +49,14 @@ public class SegmentUtil
      *  Simplifies a segment by removing points that are less than a specified distance apart
      *  (using Pythagorean distance). The returned segment is a newly-created mutable list.
      */
-    public static List<Point> simplify(List<Point> segment, double minDistanceMeters)
+    public static List<Point> simplify(List<? extends Point> segment, double minDistanceMeters)
     {
         if ((segment == null) || segment.isEmpty())
             return new ArrayList<Point>();
 
         List<Point> result = new ArrayList<Point>(segment.size());
 
-        Iterator<Point> itx = segment.iterator();
+        Iterator<? extends Point> itx = segment.iterator();
         Point prev = itx.next();
         result.add(prev);
         while (itx.hasNext())
@@ -70,5 +70,175 @@ public class SegmentUtil
         }
 
         return result;
+    }
+
+
+    /**
+     *  Attempts to find two sections of the passed segments that align.
+     *  <p>
+     *  This process starts by finding the first points in the two lists that are
+     *  no more than <code>maxSeparation</code> apart. That point in the first list
+     *  is then used to find the closest subsequent point in the second list (which
+     *  may be the one we've already found). These two points become the first pair
+     *  returned.
+     *  <p>
+     *  The first list is then iterated to find a point at least <code>minSeparation</code>
+     *  from the first, and this is then matched to the second list.
+     *  <p>
+     *  This repeats until one of the lists has no more elements, or until the
+     *  distance between the first and second lists exceeds <code>maxDivergence</code>.
+     */
+    public static List<Point[]> align(List<? extends Point> s1, List<? extends Point> s2, double minIncrement, double maxSeparation)
+    {
+        List<Point[]> result = new ArrayList<Point[]>();
+
+        AlignHelper itx1 = new AlignHelper(s1);
+        AlignHelper itx2 = new AlignHelper(s2);
+
+        // these points are outside the loop so that their previous values can be used inside
+        Point p1 = null;
+        Point p2 = null;
+        while (itx1.hasNext() && itx2.hasNext())
+        {
+            p1 = itx1.next(p1, minIncrement);
+
+            // no more points in the segment that obey separation rule
+            if (p1 == null)
+                return result;
+
+            p2 = itx2.findMatchingPoint(p1, maxSeparation);
+            if (p2 != null)
+            {
+                result.add(new Point[] {p1, p2});
+            }
+
+            // this is the end of the line ... unless we haven't started a line!
+            if (result.isEmpty())
+            {
+                itx2.returnToMark();
+            }
+        }
+
+        return result;
+    }
+
+//----------------------------------------------------------------------------
+//  Internals
+//----------------------------------------------------------------------------
+
+    /**
+     *  This class holds the segments for matching. It behaves like an iterator,
+     *  but adds functionality to seek and backtrack.
+     */
+    private static class AlignHelper
+    implements Iterator<Point>
+    {
+        private ArrayList<? extends Point> segment;
+        private int idx;
+        private int mark;
+
+        public AlignHelper(List<? extends Point> segment)
+        {
+            // null segments are converted to empty segments -- no null check in parent
+            this.segment = (segment != null)
+                         ? new ArrayList<Point>(segment)
+                         : new ArrayList<Point>();
+        }
+
+
+        /**
+         *  Returns <code>true</code> if there are any more points in the segment.
+         */
+        public boolean hasNext()
+        {
+            return idx < segment.size();
+        }
+
+
+        /**
+         *  Returns the next point in the segment, <code>null</code> if there aren't
+         *  any more (note that this differs from a normal iterator, which throws).
+         */
+        public Point next()
+        {
+            return hasNext()
+                 ? segment.get(idx++)
+                 : null;
+        }
+
+
+        /**
+         *  Returns the next point in the segment that's at least <code>minSeparation</code>
+         *  from the provided point, <code>null</code> if there isn't one.
+         */
+        public Point next(Point p, double minSeparation)
+        {
+            // this will be true when called at the start of a segment
+            if (p == null)
+                return next();
+
+            while (hasNext())
+            {
+                Point x = next();
+                if (DistanceUtil.pythagorean(p, x) >= minSeparation)
+                    return x;
+            }
+
+            return null;
+        }
+
+
+        /**
+         *  Marks the current iterator location. This is used when exploring the point space.
+         */
+        public void mark()
+        {
+            mark = idx;
+        }
+
+
+        /**
+         *  Resets the iterator to its mark. This is used when exploring the point space.
+         */
+        public void returnToMark()
+        {
+            idx = mark;
+        }
+
+
+        /**
+         *  Finds the next point in this segment that's closest to the given point, but
+         *  less than <code>maxSeparation</code> apart.
+         */
+        public Point findMatchingPoint(Point p, double maxSeparation)
+        {
+            while (hasNext())
+            {
+                Point x = next();
+                double dx = DistanceUtil.pythagorean(p, x);
+                if (dx < maxSeparation)
+                {
+                    mark();
+                    while (hasNext())
+                    {
+                        Point y = next();
+                        double dy = DistanceUtil.pythagorean(p, y);
+                        if (dy > dx)
+                        {
+                            returnToMark();
+                            return x;
+                        }
+                        else
+                        {
+                            x = y;
+                            dx = dy;
+                            mark();
+                        }
+                    }
+                    return x;
+                }
+            }
+            return null;
+        }
     }
 }
--- trunk/src/test/java/net/sf/kdgcommons/geo/TestSegmentUtil.java
+++ trunk/src/test/java/net/sf/kdgcommons/geo/TestSegmentUtil.java
@@ -41,15 +41,15 @@ public class TestSegmentUtil extends TestCase
 
     public void testSimplify() throws Exception
     {
-        Point p1 = new Point(39.95237313052457,-75.16358214083702);
-        Point p2 = new Point(39.95170387355903,-75.1636974783742);  // 75
-        Point p3 = new Point(39.95087794725416,-75.16387676482816); // 93
-        Point p4 = new Point(39.9500847988068,-75.16401850478087);  // 88
-        Point p5 = new Point(39.94931118735456,-75.16419709380426); // 87
-        Point p6 = new Point(39.94819763489783,-75.16451189922911); // 126
-        Point p7 = new Point(39.9469767093315,-75.16476658678573);  // 137
-        Point p8 = new Point(39.94556761759691,-75.16499357774308); // 157
-        Point p9 = new Point(39.94467629798356,-75.16526082455496); // 101
+        Point p1 = new Point(39.95237, -75.16358);
+        Point p2 = new Point(39.95170, -75.16369);  // 75
+        Point p3 = new Point(39.95087, -75.16387);  // 93
+        Point p4 = new Point(39.95008, -75.16401);  // 88
+        Point p5 = new Point(39.94931, -75.16419);  // 87
+        Point p6 = new Point(39.94819, -75.16451);  // 126
+        Point p7 = new Point(39.94697, -75.16476);  // 137
+        Point p8 = new Point(39.94556, -75.16499);  // 157
+        Point p9 = new Point(39.94467, -75.16526);  // 101
 
         List<Point> orig    = Arrays.asList(p1, p2, p3, p4, p5, p6, p7, p8, p9);
 
@@ -67,5 +67,296 @@ public class TestSegmentUtil extends TestCase
 
         assertEquals("passed null",  Collections.emptyList(), SegmentUtil.simplify(null, 0));
         assertEquals("passed empty", Collections.emptyList(), SegmentUtil.simplify(Collections.<Point>emptyList(), 0));
+    }
+
+
+    public void testAlignSimple() throws Exception
+    {
+        // in this test, the "B" points vary by a few meters from the "A" points
+        // so the segments should align perfectly
+
+        Point p1a = new Point(39.95237, -75.16358);
+        Point p1b = new Point(39.95239, -75.16356);
+
+        Point p2a = new Point(39.95170, -75.16369);
+        Point p2b = new Point(39.95172, -75.16371);
+
+        Point p3a = new Point(39.95087, -75.16387);
+        Point p3b = new Point(39.95084, -75.16382);
+
+        Point p4a = new Point(39.95008, -75.16401);
+        Point p4b = new Point(39.95007, -75.16400);
+
+        Point p5a = new Point(39.94931, -75.16419);
+        Point p5b = new Point(39.94935, -75.16416);
+
+        Point p6a = new Point(39.94819, -75.16451);
+        Point p6b = new Point(39.94814, -75.16452);
+
+        Point p7a = new Point(39.94697, -75.16476);
+        Point p7b = new Point(39.94699, -75.16475);
+
+        Point p8a = new Point(39.94556, -75.16499);
+        Point p8b = new Point(39.94558, -75.16492);
+
+        Point p9a = new Point(39.94467, -75.16526);
+        Point p9b = new Point(39.94466, -75.16522);
+
+        List<Point> segmentA = Arrays.asList(p1a, p2a, p3a, p4a, p5a, p6a, p7a, p8a, p9a);
+        List<Point> segmentB = Arrays.asList(p1b, p2b, p3b, p4b, p5b, p6b, p7b, p8b, p9b);
+
+        List<Point[]> result = SegmentUtil.align(segmentA, segmentB, 10, 100);
+
+        assertEquals("matched all points", segmentA.size(), result.size());
+        for (int ii = 0 ; ii < result.size() ; ii++)
+        {
+            assertEquals("segmentA[" + ii + "]", segmentA.get(ii), result.get(ii)[0]);
+            assertEquals("segmentB[" + ii + "]", segmentB.get(ii), result.get(ii)[1]);
+        }
+    }
+
+
+    public void testFirstSegmentShorter() throws Exception
+    {
+        // in this test, the "B" segment has excess points, which are ignored
+
+        Point p1a = new Point(39.95237, -75.16358);
+        Point p1b = new Point(39.95239, -75.16356);
+
+        Point p2a = new Point(39.95170, -75.16369);
+        Point p2b = new Point(39.95172, -75.16371);
+
+        Point p3a = new Point(39.95087, -75.16387);
+        Point p3b = new Point(39.95084, -75.16382);
+
+        Point px = new Point(39.94900, -75.16356);
+        Point py = new Point(39.94800, -75.16356);
+        Point pz = new Point(39.98700, -75.16356);
+
+        List<Point> segmentA = Arrays.asList(p1a, p2a, p3a);
+        List<Point> segmentB = Arrays.asList(p1b, p2b, p3b, px, py, pz);
+
+        List<Point[]> result = SegmentUtil.align(segmentA, segmentB, 10, 100);
+
+        assertEquals("matched all points", 3, result.size());
+        for (int ii = 0 ; ii < result.size() ; ii++)
+        {
+            assertEquals("segmentA[" + ii + "]", segmentA.get(ii), result.get(ii)[0]);
+            assertEquals("segmentB[" + ii + "]", segmentB.get(ii), result.get(ii)[1]);
+        }
+    }
+
+
+    public void testSecondSegmentShorter() throws Exception
+    {
+        // in this test, the "A" segment has excess points, which are ignored
+
+        Point p1a = new Point(39.95237, -75.16358);
+        Point p1b = new Point(39.95239, -75.16356);
+
+        Point p2a = new Point(39.95170, -75.16369);
+        Point p2b = new Point(39.95172, -75.16371);
+
+        Point p3a = new Point(39.95087, -75.16387);
+        Point p3b = new Point(39.95084, -75.16382);
+
+        Point px = new Point(39.94900, -75.16356);
+        Point py = new Point(39.94800, -75.16356);
+        Point pz = new Point(39.98700, -75.16356);
+
+        List<Point> segmentA = Arrays.asList(p1a, p2a, p3a, px, py, pz);
+        List<Point> segmentB = Arrays.asList(p1b, p2b, p3b);
+
+        List<Point[]> result = SegmentUtil.align(segmentA, segmentB, 10, 100);
+
+        assertEquals("matched all points", 3, result.size());
+        for (int ii = 0 ; ii < result.size() ; ii++)
+        {
+            assertEquals("segmentA[" + ii + "]", segmentA.get(ii), result.get(ii)[0]);
+            assertEquals("segmentB[" + ii + "]", segmentB.get(ii), result.get(ii)[1]);
+        }
+    }
+
+
+    public void testFindStart1() throws Exception
+    {
+        // in this test, the 1st "A" point corresponds to the 4th "B" point
+
+        Point px = new Point(39.95600, -75.16356);
+        Point py = new Point(39.95500, -75.16356);
+        Point pz = new Point(39.95400, -75.16356);
+
+        Point p1a = new Point(39.95237, -75.16358);
+        Point p1b = new Point(39.95239, -75.16356);
+
+        Point p2a = new Point(39.95170, -75.16369);
+        Point p2b = new Point(39.95172, -75.16371);
+
+        Point p3a = new Point(39.95087, -75.16387);
+        Point p3b = new Point(39.95084, -75.16382);
+
+        List<Point> segmentA = Arrays.asList(p1a, p2a, p3a);
+        List<Point> segmentB = Arrays.asList(px, py, pz, p1b, p2b, p3b);
+
+        List<Point[]> result = SegmentUtil.align(segmentA, segmentB, 10, 100);
+
+        assertEquals("matched all points", 3, result.size());
+        for (int ii = 0 ; ii < result.size() ; ii++)
+        {
+            assertEquals("segmentA[" + ii + "]", segmentA.get(ii), result.get(ii)[0]);
+            assertEquals("segmentB[" + ii + "]", segmentB.get(ii + 3), result.get(ii)[1]);
+        }
+    }
+
+
+    public void testFindStart2() throws Exception
+    {
+        // in this test, the 4th "A" point corresponds to the 1st "B" point
+
+        Point px = new Point(39.95600, -75.16356);
+        Point py = new Point(39.95500, -75.16356);
+        Point pz = new Point(39.95400, -75.16356);
+
+        Point p1a = new Point(39.95237, -75.16358);
+        Point p1b = new Point(39.95239, -75.16356);
+
+        Point p2a = new Point(39.95170, -75.16369);
+        Point p2b = new Point(39.95172, -75.16371);
+
+        Point p3a = new Point(39.95087, -75.16387);
+        Point p3b = new Point(39.95084, -75.16382);
+
+        List<Point> segmentA = Arrays.asList(px, py, pz, p1a, p2a, p3a);
+        List<Point> segmentB = Arrays.asList(p1b, p2b, p3b);
+
+        List<Point[]> result = SegmentUtil.align(segmentA, segmentB, 10, 100);
+
+        assertEquals("matched all points", 3, result.size());
+        for (int ii = 0 ; ii < result.size() ; ii++)
+        {
+            assertEquals("segmentA[" + ii + "]", segmentA.get(ii + 3), result.get(ii)[0]);
+            assertEquals("segmentB[" + ii + "]", segmentB.get(ii), result.get(ii)[1]);
+        }
+    }
+
+
+    public void testMinimumIncrement() throws Exception
+    {
+        // in this test every "A" point has a friend that's less than the
+        // minimum incremental distance, so should be skipped
+
+        Point p1a  = new Point(39.95237, -75.16358);
+        Point p1ax = new Point(39.95200, -75.16358);
+        Point p1b  = new Point(39.95239, -75.16356);
+
+        Point p2a  = new Point(39.95170, -75.16369);
+        Point p2ax = new Point(39.95140, -75.16369);
+        Point p2b  = new Point(39.95172, -75.16371);
+
+        Point p3a  = new Point(39.95087, -75.16387);
+        Point p3ax = new Point(39.95047, -75.16387);
+        Point p3b  = new Point(39.95084, -75.16382);
+
+        Point p4a  = new Point(39.95008, -75.16401);
+        Point p4ax = new Point(39.94988, -75.16401);
+        Point p4b  = new Point(39.95007, -75.16400);
+
+        Point p5a  = new Point(39.94931, -75.16419);
+        Point p5ax = new Point(39.94911, -75.16419);
+        Point p5b  = new Point(39.94935, -75.16416);
+
+        Point p6a = new Point(39.94819, -75.16451);
+        Point p6b = new Point(39.94814, -75.16452);
+
+        List<Point> segmentA = Arrays.asList(p1a, p1ax, p2a, p2ax, p3a, p3ax, p4a, p4ax, p5a, p5ax, p6a);
+        List<Point> segmentB = Arrays.asList(p1b,       p2b,       p3b,       p4b,       p5b,       p6b);
+
+        List<Point[]> result = SegmentUtil.align(segmentA, segmentB, 50, 100);
+
+        assertEquals("matched all points", segmentB.size(), result.size());
+        for (int ii = 0 ; ii < result.size() ; ii++)
+        {
+            assertEquals("segmentA[" + ii * 2 + "]", segmentA.get(ii * 2), result.get(ii)[0]);
+            assertEquals("segmentB[" + ii + "]",     segmentB.get(ii),     result.get(ii)[1]);
+        }
+    }
+
+
+    public void testClosestPartner() throws Exception
+    {
+        // in this test there are two "B" points for each "A", and one of them
+        // is closer than the other
+
+        Point p1a  = new Point(39.95237, -75.16358);
+        Point p1b  = new Point(39.95239, -75.16356);
+
+        Point p2a  = new Point(39.95170, -75.16369);
+        Point p2bx = new Point(39.95200, -75.16358);
+        Point p2b  = new Point(39.95172, -75.16371);
+
+        Point p3a  = new Point(39.95087, -75.16387);
+        Point p3bx = new Point(39.95140, -75.16369);
+        Point p3b  = new Point(39.95084, -75.16382);
+
+        Point p4a  = new Point(39.95008, -75.16401);
+        Point p4bx = new Point(39.95047, -75.16387);
+        Point p4b  = new Point(39.95007, -75.16400);
+
+        Point p5a  = new Point(39.94931, -75.16419);
+        Point p5bx = new Point(39.94988, -75.16401);
+        Point p5b  = new Point(39.94935, -75.16416);
+
+        Point p6a = new Point(39.94819, -75.16451);
+        Point p6bx = new Point(39.94911, -75.16419);
+        Point p6b = new Point(39.94814, -75.16452);
+
+        List<Point> segmentA = Arrays.asList(p1a,       p2a,       p3a,       p4a,       p5a,       p6a);
+        List<Point> segmentB = Arrays.asList(p1b, p2bx, p2b, p3bx, p3b, p4bx, p4b, p5bx, p5b, p6bx, p6b);
+
+        List<Point[]> result = SegmentUtil.align(segmentA, segmentB, 50, 100);
+
+        assertEquals("matched all points", segmentA.size(), result.size());
+        for (int ii = 0 ; ii < result.size() ; ii++)
+        {
+            assertEquals("segmentA[" + ii + "]",     segmentA.get(ii), result.get(ii)[0]);
+            assertEquals("segmentB[" + ii * 2 + "]", segmentB.get(ii * 2),     result.get(ii)[1]);
+        }
+    }
+
+
+    public void testDivergenceSimple() throws Exception
+    {
+        // P4 A/B diverge by less than 100 meters, P5 by more, and P6 rejoin but should
+        // be ignored
+
+        Point p1a = new Point(39.95237, -75.16358);
+        Point p1b = new Point(39.95239, -75.16356);
+
+        Point p2a = new Point(39.95170, -75.16369);
+        Point p2b = new Point(39.95172, -75.16371);
+
+        Point p3a = new Point(39.95087, -75.16387);
+        Point p3b = new Point(39.95084, -75.16382);
+
+        Point p4a = new Point(39.95008, -75.16401);
+        Point p4b = new Point(39.95007, -75.16350);
+
+        Point p5a = new Point(39.95008, -75.16475);
+        Point p5b = new Point(39.95007, -75.16310);
+
+        Point p6a = new Point(39.94819, -75.16451);
+        Point p6b = new Point(39.94814, -75.16452);
+
+        List<Point> segmentA = Arrays.asList(p1a, p2a, p3a, p4a, p5a, p6a);
+        List<Point> segmentB = Arrays.asList(p1b, p2b, p3b, p4b, p5b, p6b);
+
+        List<Point[]> result = SegmentUtil.align(segmentA, segmentB, 10, 100);
+
+        assertEquals("matched all points", 4, result.size());
+        for (int ii = 0 ; ii < result.size() ; ii++)
+        {
+            assertEquals("segmentA[" + ii + "]", segmentA.get(ii), result.get(ii)[0]);
+            assertEquals("segmentB[" + ii + "]", segmentB.get(ii), result.get(ii)[1]);
+        }
     }
 }
