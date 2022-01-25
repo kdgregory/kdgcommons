e786fb768dc39295b955a44d6a204afcc5f44ef2
Keith Gregory <contact@kdgregory.com>
2022-01-25 07:49:50 -0500

    HtmlUtil and URLBuilder now in util package; deprecate current versions

--- src/test/java/net/sf/kdgcommons/util/TestURLBuilder.java
+++ src/test/java/net/sf/kdgcommons/util/TestURLBuilder.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package net.sf.kdgcommons.html;
+package net.sf.kdgcommons.util;
 
 import junit.framework.TestCase;
 
